c687740ff430942c0f1beef7a743f993fbbfc6ce
Rexon Julianto <rexonindo@gmail.com>
2025-12-10 23:23:45 +0700

    Update View Progress WO

---
+++ database/migrations/2025_12_10_231921_create_wo_progress_view_view.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("CREATE VIEW `wo_progress_view` AS select `a`.`wo_no` AS `wo_no`,`a`.`itm_cd` AS `itm_cd`,`b`.`itm_type` AS `itm_type`,`c`.`seq_no` AS `seq_no`,`c`.`proc_cd` AS `proc_cd`,`d`.`proc_nm` AS `proc_nm`,`e`.`start_time` AS `start_time`,`e`.`end_time` AS `end_time`,`a`.`plan_qty` AS `wo_qty`,ifnull(`c`.`cav`,0) AS `cav`,(`e`.`in_qty` * ifnull(`c`.`cav`,0)) AS `in_qty`,(`e`.`rwk_qty` * ifnull(`c`.`cav`,0)) AS `rwk_qty`,(`e`.`ng_qty` * ifnull(`c`.`cav`,0)) AS `ng_qty`,(`e`.`out_qty` * ifnull(`c`.`cav`,0)) AS `out_qty`,`e`.`out_qty` AS `out_qty_shoot`,`e`.`mchn_cd` AS `mchn_cd`,`g`.`emp_nm` AS `emp_nm` from (((((`mais_wellbest`.`wo_tbl` `a` left join `mais_wellbest`.`itm_tbl` `b` on((`a`.`itm_cd` = `b`.`itm_cd`))) left join `mais_wellbest`.`wo_proc_tbl` `c` on((`a`.`wo_no` = `c`.`wo_no`))) left join `mais_wellbest`.`proc_tbl` `d` on((`c`.`proc_cd` = `d`.`proc_cd`))) left join `mais_wellbest`.`prdlog_tbl` `e` on(((`a`.`wo_no` = `e`.`wo_no`) and (`a`.`itm_cd` = `e`.`itm_cd`) and (`c`.`proc_cd` = `e`.`proc_cd`)))) left join `mais_wellbest`.`empl_tbl` `g` on((`e`.`emp_id` = `g`.`emp_id`))) order by `a`.`wo_no`,`a`.`itm_cd`,`b`.`itm_type`,`c`.`seq_no`");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement("DROP VIEW IF EXISTS `wo_progress_view`");
+    }
+};
--- storage/framework/views/d038d3fb79594d22d222cf6b7a321c3e.php
+++
@@ -1,7 +0,0 @@
-<!--[if BLOCK]><![endif]--><?php $__currentLoopData = $getActions(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $action): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <!--[if BLOCK]><![endif]--><?php if($action->isVisible()): ?>
-        <?php echo e($action); ?>
-
-    <?php endif; ?><!--[if ENDBLOCK]><![endif]-->
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?><!--[if ENDBLOCK]><![endif]-->
-<?php /**PATH D:\website\mais-wellbest\vendor\filament\forms\resources\views/components/actions/action-container.blade.php ENDPATH**/ ?>
